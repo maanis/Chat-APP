61bf920bac8363f4ac819f62cf4dba098e5f0ae1
Manish Jha <manishjha962@gmail.com>
2024-12-03 16:14:17 +0530

    multerConfig

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,11 @@ router.get('/', async function (req, res, next) {
     let error = req.flash('error')
     res.render('index', {error});
 });
+
+router.get('/dashboard', async function (req, res, next) {
+    res.render('dashboard')
+});
+
 router.post('/register', register)
 router.post('/login', login)
 
