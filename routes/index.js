a18456d070426f9aa2ed13ed7ea4d5599d117c30
Manish Jha <manishjha962@gmail.com>
2024-12-04 06:39:25 +0530

    day 2

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const upload = require('../config/multer-config');
 const { isLoggedIn } = require('../middlewares/isLoggedIn');
 const userModel = require('../models/userModel');
 var router = express.Router();
-/* GET home page. */
+
 
 router.get('/', async function (req, res, next) {
     let error = req.flash('error')
@@ -20,6 +20,7 @@ router.get('/dashboard', isLoggedIn,async function (req, res, next) {
 router.post('/register', upload.single('image'), register)
 
 router.post('/login', login)
+
 
 router.get('/logout', logout)
 
